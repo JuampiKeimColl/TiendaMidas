0c2a109bc176e02798530d47bd1dbab167b8da97
JuampiKeim <juampikeim@gmail.com>
2023-09-26 14:16:51 -0300

    Primer commit

---
+++ src/test/java/com/midas/tienda/TiendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.midas.tienda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
